d77673657ee1350f63fa8e9a4c57fe69cf661e4d
Dan <dalbertico007@gmail.com>
2024-07-12 14:58:53 -0700

    2d plotting added

--- graphmaker.py
+++ graphmaker.py
@@ -1,106 +1,78 @@
 import numpy as np
 import igraph as ig
 from tqdm import tqdm
-import rdimacs as rd 
+import rdimacs as rd
 import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 
-# Updated to igraph because it is straight up faster.
-
-def make_graph_positions_optimized(filename, max_edges=100000): #disable max_edges if you have a beefy machine
+def make_graph_positions_optimized(filename, max_nodes=10000, max_edges=50000):
     num_vars, clauses = rd.read_dimacs(filename)
-    G = generate_interaction_graph_optimized(num_vars, clauses)
+    print(f"Variables: {num_vars}, Clauses: {len(clauses)}")
     
+    G = generate_interaction_graph_optimized(num_vars, clauses, max_nodes, max_edges)
+   
     print("Calculating layout...")
-    pos = G.layout_drl(dim=3)
+    pos = G.layout_fruchterman_reingold(dim=3, weights='weight', niter=50)
     pos = np.array(pos)
-    
-    Xn, Yn, Zn = pos.T
-    labels = [str(k+1) for k in range(2 * num_vars)]
-    
-    color_mapping = {label: idx for idx, label in enumerate(labels)}
-    node_colors = np.arange(2 * num_vars)
-    
+   
     print("Preparing edge data...")
     edges = np.array(G.get_edgelist())
-    
-    # Downsample edges if there are too many
-    if len(edges) > max_edges:
-        downsample_rate = len(edges) // max_edges
-        edges = edges[::downsample_rate]
-    
-    Xe = np.empty((len(edges) * 3,))
-    Ye = np.empty((len(edges) * 3,))
-    Ze = np.empty((len(edges) * 3,))
-    
-    Xe[::3], Xe[1::3], Xe[2::3] = pos[edges[:, 0], 0], pos[edges[:, 1], 0], None
-    Ye[::3], Ye[1::3], Ye[2::3] = pos[edges[:, 0], 1], pos[edges[:, 1], 1], None
-    Ze[::3], Ze[1::3], Ze[2::3] = pos[edges[:, 0], 2], pos[edges[:, 1], 2], None
-    
-    return Xn, Yn, Zn, labels, color_mapping, node_colors, Xe, Ye, Ze
+   
+    return pos, edges, min(num_vars, max_nodes // 2)
 
-def generate_interaction_graph_optimized(num_vars, clauses):
-    edges = set()
+def generate_interaction_graph_optimized(num_vars, clauses, max_nodes, max_edges):
+    edge_dict = {}
+    node_set = set()
     for clause in tqdm(clauses, desc="Generating graph"):
-        clause_edges = set((literal_to_id(a, num_vars), literal_to_id(b, num_vars))
-                           for i, a in enumerate(clause)
-                           for b in clause[i+1:])
-        edges.update(clause_edges)
-    
-    G = ig.Graph(n=2*num_vars, edges=list(edges))
+        for i, a in enumerate(clause):
+            for b in clause[i+1:]:
+                node1, node2 = literal_to_id(a, num_vars, max_nodes), literal_to_id(b, num_vars, max_nodes)
+                if node1 != node2:
+                    edge = tuple(sorted([node1, node2]))
+                    edge_dict[edge] = edge_dict.get(edge, 0) + 1
+                    node_set.add(node1)
+                    node_set.add(node2)
+                if len(edge_dict) >= max_edges:
+                    break
+            if len(edge_dict) >= max_edges:
+                break
+        if len(edge_dict) >= max_edges:
+            break
+
+    G = ig.Graph(n=len(node_set), edges=list(edge_dict.keys()), edge_attrs={'weight': list(edge_dict.values())})
     return G
 
-def literal_to_id(literal, num_vars):
-    """Maps a literal to a unique node ID."""
+def literal_to_id(literal, num_vars, max_nodes):
+    """Maps a literal to a unique non-negative node ID within the max_nodes limit."""
     if literal > 0:
-        return literal - 1  # Positive literals: 0 to num_vars-1
+        return (literal - 1) % (max_nodes // 2)
     else:
-        return num_vars - literal - 1  # Negative literals: num_vars to 2*num_vars-1
+        return (max_nodes // 2 + abs(literal) - 1) % max_nodes
 
-def plot_graph_from_positions(Xn, Yn, Zn, labels, color_mapping, node_colors, Xe, Ye, Ze):
-    trace1 = go.Scatter3d(
-        x=Xe,
-        y=Ye,
-        z=Ze,
-        mode='lines',
-        line=dict(color='rgb(125,125,125)', width=1),
-        hoverinfo='none'
+def plot_graph_from_positions(pos, edges, num_vars, max_edges_to_plot=1000000):
+    Xn, Yn, Zn = pos.T
+   
+    edge_x = []
+    edge_y = []
+    edge_z = []
+   
+    for i, edge in enumerate(edges[:max_edges_to_plot]):
+        edge_x.extend([pos[edge[0], 0], pos[edge[1], 0], None])
+        edge_y.extend([pos[edge[0], 1], pos[edge[1], 1], None])
+        edge_z.extend([pos[edge[0], 2], pos[edge[1], 2], None])
+   
+    node_colors = ['blue' if i < num_vars else 'red' for i in range(len(Xn))]
+   
+    fig = make_subplots(rows=1, cols=1, specs=[[{'type': 'scene'}]])
+   
+    fig.add_trace(go.Scatter3d(x=edge_x, y=edge_y, z=edge_z, mode='lines', line=dict(color='lightgrey', width=1), hoverinfo='none'), row=1, col=1)
+    fig.add_trace(go.Scatter3d(x=Xn, y=Yn, z=Zn, mode='markers', marker=dict(size=4, color=node_colors), hoverinfo='text', text=[f'Var {i+1}' if i < num_vars else f'Not Var {i-num_vars+1}' for i in range(len(Xn))]), row=1, col=1)
+   
+    fig.update_layout(
+        title="3D Visualization of CNF Variables and Clauses (Simplified)",
+        scene=dict(xaxis_title="X", yaxis_title="Y", zaxis_title="Z"),
+        width=800, height=800,
+        margin=dict(r=20, l=10, b=10, t=40)
     )
-
-    trace2 = go.Scatter3d(
-        x=Xn,
-        y=Yn,
-        z=Zn,
-        mode='markers',
-        name='variables',
-        marker=dict(symbol='circle', size=6, color=node_colors, colorscale='Viridis', line=dict(color='rgb(50,50,50)', width=0.5)),
-        text=labels,
-        hoverinfo='text'
-    )
-
-    axis = dict(showbackground=False, showline=False, zeroline=False, showgrid=False, showticklabels=False, title='')
-
-    layout = go.Layout(
-        title="3D Visualization of CNF Variables and Clauses",
-        width=1000,
-        height=1000,
-        showlegend=False,
-        scene=dict(xaxis=dict(axis), yaxis=dict(axis), zaxis=dict(axis)),
-        margin=dict(t=100),
-        hovermode='closest',
-        annotations=[
-            dict(
-                showarrow=False,
-                text="Data source: <a href='your_data_source_url'>CNF File</a>",
-                xref='paper',
-                yref='paper',
-                x=0,
-                y=0.1,
-                xanchor='left',
-                yanchor='bottom',
-                font=dict(size=14)
-            )
-        ]
-    )
-
-    fig = go.Figure(data=[trace1, trace2], layout=layout)
+   
     fig.show()
